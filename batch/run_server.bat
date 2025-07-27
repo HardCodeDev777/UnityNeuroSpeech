5416b28451054acc72b36dfeeb307da7e1826f4f
HardCodeDev <hardcodedev777@yandex.com>
2025-07-27 15:40:38 +0400

    Update TTS Server
    - Fix requirements.txt
    - Fix run_server.bat
    - Enable debug for main.py
    - Improve main.py code readability

--- batch/run_server.bat
+++ batch/run_server.bat
@@ -1,12 +1,6 @@
 @echo off
 cd /d %~dp0\Server
 
-echo *************************************************
-echo UnityNeuroSpeech Text-To-Speech server is running!
-echo UnityNeuroSpeech official GitHub repository: https://github.com/HardCodeDev777/UnityNeuroSpeech
-echo *************************************************
-
-call .venv\Scripts\activate
-python main.py
+.venv\Scripts\python.exe main.py
 
 pause
